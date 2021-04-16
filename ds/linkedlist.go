d1e1d4816d1cd5033d1c59033592816c0036c98d
Dedi Prakasa <dedprakasa@gmail.com>
2021-04-16 09:58:10 +0000

    modify linkedlist

--- ds/linkedlist.go
+++ ds/linkedlist.go
@@ -3,23 +3,23 @@ package main
 import "fmt"
 
 type Node struct {
-  Value int
-  Next *Node
+  value int
+  next *Node
 }
 
 type LinkedList struct {
-  Head *Node
-  Tail *Node
+  head *Node
+  tail *Node
 }
 
 func (l *LinkedList) push(node *Node) {
-  l.Tail.Next = node
+  l.tail.next = node
 }
 
 func main() {
   linkedList := LinkedList{}
-  node1 := &Node{Value: 1}
-  linkedList.Head = node1
+  node1 := &Node{value: 1}
+  linkedList.head = node1
 
-  fmt.Println(linkedList.Head.Value)
+  fmt.Println(linkedList.head.value)
 }
